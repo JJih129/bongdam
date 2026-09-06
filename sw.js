288296575750f9867156364f1acb60eb4479aa82
JJih129 <sjh012916@gmail.com>
2026-09-06 09:18:30 +0900

    웹 게시 v398m — 안전지도 버튼 세로 쪼개짐 수정

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="bongdam-1e09f2369a";
+const CACHE="bongdam-91e7b5cc38";
 const CORE=["index.html","manifest.webmanifest"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
